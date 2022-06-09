dc2caed48f7fa464ae66a6088743143f2ee493da
mrcaseyrobison <104058949+mrcaseyrobison@users.noreply.github.com>
2022-06-09 12:52:44 -0700

    not working code

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ var special = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ",", 
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Concat Array
-var allCharacters =[];
-
 // Variable Declerations
 var userChoiceLength ="";
 var userChoiceNumbers;
@@ -18,21 +15,65 @@ var userChoiceSpecial;
 var userChoiceLower;
 var userChoiceUpper;
 
-//////////////
+// Console Log to Confirm User Choice
+console.log ("Password Length: " + userChoiceLength);
+console.log ("Number Choice: " + userChoiceNumbers);
+console.log ("Special Characters Choice: " + userChoiceSpecial);
+console.log ("Lower Case Letters Choice: " + userChoiceSpecial);
+console.log ("Upper Case Letters Choice: " + userChoiceSpecial);
+console.log ("Perameter Options by User Choice: " + allCharacters);
+console.log ("User Password: " + userPassword)
+
+// Begin Password Functions
 
 function generatePassword() {
-        var userChoiceLength = (prompt ("How long do you want your password? Choose between 8 and 128."))
-if (userChoiceLength >= 8 || userChoiceLength <=128) { 
+        userChoiceLength = (prompt ("How long do you want your password? Choose between 8 and 128."))
+if (userChoiceLength < 8 || userChoiceLength > 128) { 
     alert ("Password must be between 8 and 128 characters")
     return;
 }
+
+if (userChoiceLength >=8 && userChoiceLength <= 128) { 
+    userChoiceNumbers = window.confirm ("Click OK to use numbers");
+    userChoiceSpecial = window.confirm ("Click OK to use special characters");
+    userChoiceLower = window.confirm ("Click OK to use lower case letters");
+    userChoiceUpper = window.confirm ("Click OK to use upper case letters");
+
+if (userChoiceNumbers == false && userChoiceSpecial == false && userChoiceLower == false && userChoiceUpper == false) {
+    alert ("You must choose at least one perameter")
+    return;
 }
-// if (userChoiceLength )
-//     var userChoiceNumbers = confirm ("Click OK to use numbers")
-//     var userChoiceSpecial = confirm ("Click OK to use special characters")
-//     var userChoiceLower = confirm ("Click OK to use lower case letters")
-//     var userChoiceUpper = confirm ("Click OK to use upper case letters")
-    
+}
+
+// Concat Array
+var allCharacters =[];
+
+if (userChoiceNumbers) {
+    allCharacters = allCharacters.concat(userChoiceNumbers)
+}
+
+if (userChoiceSpecial) {
+    allCharacters = allCharacters.concat(userChoiceSpecial)
+}
+
+if (userChoiceLower) {
+    allCharacters = allCharacters.concat(userChoiceLower)
+}
+
+if (userChoiceUpper) {
+    allCharacters = allCharacters.concat(userChoiceUpper)
+}
+
+var userPassword = ""
+
+for (var i = 0; i < userChoiceLength; i++) {
+    userPassword = userPassword + allCharacters [Math.floor(Math.random() * allCharacters.length)];
+}
+
+return userPassword;
+
+} 
+
 
 // Define Goal
 function writePassword() {
@@ -41,4 +82,3 @@ function writePassword() {
 
     passwordText.value = password;
 }
-
